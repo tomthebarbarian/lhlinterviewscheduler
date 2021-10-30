926e36723f6f2ddce56f82abe4ff80de9c0e898b
Tom Zhang <tom.zhang989@gmail.com>
2021-10-29 20:33:17 -0400

    transition and back working better now

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,17 +4,28 @@ export default function useVisualMode(initial){
   const [mode, setMode] = useState(initial)
   const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode){
+  const transition = function(newMode,replace){
     setMode(newMode)
-    const modeCopy = mode
-    setHistory(prev => [...prev, modeCopy])
+    if (replace) {
+      setHistory(prev => {
+        const prevCopy = prev
+        const popped = prevCopy.pop()
+        // console.log('this is prev copy',prevCopy)
+        return [...prev, newMode]
+      })
+    } else {
+      setHistory(prev => [...prev, newMode])
+    }
+    // console.log("here's history", history)
   };
 
   const back = () => {
     const historyCopy = history;
-    const poppedVal = historyCopy.pop();
-    setHistory(historyCopy)
-    setMode(poppedVal)
+    if (historyCopy.length > 1){
+      const poppedVal = historyCopy.pop();
+      setHistory(historyCopy)
+    }
+    setMode(historyCopy[historyCopy.length - 1])
   }
   return {mode, transition, back}
 };
